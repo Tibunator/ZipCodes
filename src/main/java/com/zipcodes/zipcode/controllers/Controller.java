b3b53dfc9e7fe230cd0cf8d261e620f7adc00557
Tibunator <36676808+Tibunator@users.noreply.github.com>
2020-03-31 13:02:14 -0700

    Update Controller.java

--- src/main/java/com/zipcodes/zipcode/controllers/Controller.java
+++ src/main/java/com/zipcodes/zipcode/controllers/Controller.java
@@ -2,13 +2,15 @@ package com.zipcodes.zipcode.controllers;
 
 import java.util.List;
 
+
 import com.zipcodes.zipcode.models.entity.ZipCode;
 import com.zipcodes.zipcode.services.IZipCodeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-
-
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -22,8 +24,18 @@ public class Controller {
         return zipCodeService.findAll();
     }
     @GetMapping(value="/ZipCodes/{zipcode}")
-    public ZipCode GetById(@PathVariable Integer zipcode) {
-        return zipCodeService.findByZipCode(zipcode);
+    public ResponseEntity<ZipCode> GetById(@PathVariable Integer zipcode) {
+        ZipCode zip_code=zipCodeService.findByZipCode(zipcode);
+        if (zip_code!=null){
+            return new ResponseEntity<ZipCode>(zip_code, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+    @ExceptionHandler(NumberFormatException.class)
+    public ResponseEntity<String> handleException(NumberFormatException ex)
+    {
+        return new ResponseEntity<String>("It needs to recive a Ineger value",HttpStatus.BAD_REQUEST);
     }
     
 }
