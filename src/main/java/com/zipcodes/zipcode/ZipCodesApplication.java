b6ddc1e9b62e52fd206e7366cc100cda336b2133
Tibunator <36676808+Tibunator@users.noreply.github.com>
2020-04-01 20:52:53 -0700

    Update ZipCodesApplication.java

--- src/main/java/com/zipcodes/zipcode/ZipCodesApplication.java
+++ src/main/java/com/zipcodes/zipcode/ZipCodesApplication.java
@@ -2,9 +2,10 @@ package com.zipcodes.zipcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ZipCodesApplication {
+public class ZipCodesApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZipCodesApplication.class, args);
